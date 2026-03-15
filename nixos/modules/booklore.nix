f70392709f714e660d3eaf04e6a5f2d962852f01
dnaq <dnaq@users.noreply.github.com>
2026-03-15 15:39:28 +0100

    initial service and test vm

---
+++ nixos/modules/booklore.nix
@@ -0,0 +1,124 @@
+# nixos/modules/booklore.nix
+self:
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+
+with lib;
+
+let
+  cfg = config.services.booklore;
+in
+{
+  options.services.booklore = {
+    enable = mkEnableOption "Booklore service";
+
+    user = lib.mkOption {
+      type = lib.types.str;
+      default = "booklore";
+    };
+
+    group = lib.mkOption {
+      type = lib.types.str;
+      default = "booklore";
+    };
+
+    package = lib.mkOption {
+      type = lib.types.package;
+      default = self.packages.${pkgs.system}.booklore;
+      description = "Booklore package";
+    };
+
+    database = {
+      host = mkOption {
+        type = types.str;
+        default = "127.0.0.1";
+      };
+
+      password = mkOption {
+        type = types.str;
+      };
+
+      name = mkOption {
+        type = types.str;
+        default = "booklore";
+      };
+
+      user = mkOption {
+        type = types.str;
+        default = "booklore";
+      };
+    };
+  };
+
+  config = mkIf cfg.enable {
+    users.users.${cfg.user} = {
+      isSystemUser = true;
+      inherit (cfg) group;
+      home = "/var/lib/booklore";
+      createHome = true;
+    };
+    users.groups.${cfg.group} = { };
+
+    services.mysql = {
+      enable = mkDefault true;
+      package = mkDefault pkgs.mariadb;
+    };
+
+    systemd.services.booklore-init-db = {
+      wants = [ "mysql.service" ];
+      after = [ "mysql.service" ];
+      wantedBy = [ "multi-user.target" ];
+      serviceConfig = {
+        Type = "oneshot";
+        RemainAfterExit = true;
+        User = "root";
+      };
+      script = ''
+        ${config.services.mysql.package}/bin/mysql -u root -e "
+          CREATE DATABASE IF NOT EXISTS ${cfg.database.name};
+          DROP USER IF EXISTS '${cfg.database.user}'@'localhost';
+          CREATE USER '${cfg.database.user}'@'localhost' IDENTIFIED BY '${cfg.database.password}';
+          GRANT ALL PRIVILEGES ON ${cfg.database.name}.* TO '${cfg.database.user}'@'localhost';
+          FLUSH PRIVILEGES;
+        "
+      '';
+    };
+
+    systemd.tmpfiles.rules = [
+      "d /var/lib/booklore/data 0755 booklore booklore -"
+      "d /var/lib/booklore/books 0755 booklore booklore -"
+      "d /var/lib/booklore/bookdrop 0755 booklore booklore -"
+    ];
+
+    systemd.services.booklore = {
+      description = "Booklore";
+      wantedBy = [ "multi-user.target" ];
+      wants = [
+        "mysql.service"
+        "network-online.target"
+      ];
+      serviceConfig = {
+        User = cfg.user;
+        Group = cfg.group;
+        ExecStart = "${cfg.package}/bin/booklore";
+        BindPaths = [
+          "/var/lib/booklore/data:/app/data"
+          "/var/lib/booklore/books:/books"
+          "/var/lib/booklore/bookdrop:/bookdrop"
+        ];
+        BindReadOnlyPaths = [
+          "/nix/store:/nix/store"
+        ];
+      };
+      environment = {
+        DATABASE_URL = "jdbc:mariadb://127.0.0.1:3306/booklore";
+        DATABASE_USERNAME = cfg.database.user;
+        DATABASE_PASSWORD = cfg.database.password;
+      };
+    };
+  };
+}
